96122ddb6546af17e49d06abba5a4c2cd16a4aeb
Anton <antonovhosted@inbox.ru>
2023-01-24 21:52:20 +0300

    Configured Database

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    id_user = models.IntegerField()
+    profile_img = models.ImageField(
+        upload_to='profile_images', default='blank profile image.png')
+    wb_keywords = models.TextField(blank=True)
+    ozon_keywords = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
--- core/views.py
+++ core/views.py
@@ -1,6 +1,78 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def register(request):
+
+    if request.method == 'POST':
+        username = request.POST['register_username']
+        email = request.POST['register_email']
+        pw1 = request.POST['register_pw']
+        pw2 = request.POST['register_repeat_pw']
+
+        if pw1 != pw2:
+            messages.info(request, 'Пароли не совпадают')
+            return redirect('register')
+
+        if User.objects.filter(email=email).exists():
+            messages.info(request, 'Такая почта уже используется')
+            return redirect('register')
+
+        if User.objects.filter(username=username).exists():
+            messages.info(request, 'Такой логин уже использутеся')
+            return redirect('register')
+
+        user = User.objects.create_user(
+            username=username, email=email, password=pw1)
+        user.save()
+
+        # log user in
+        user = auth.authenticate(username=username, password=pw1)
+        auth.login(request, user)
+
+        # create a Profile object for a new user
+        user_model = User.objects.get(username=username)
+        new_profile = Profile.objects.create(
+            user=user_model, email=user_model.email, id_user=user_model.id)
+        new_profile.save()
+
+        return redirect('home')
+
+    return render(request, 'register.html')
+
+
+def login(request):
+
+    if (request.method == 'POST'):
+
+        login = request.POST['login_id']
+        pw = request.POST['login_pw']
+
+        user = auth.authenticate(username=login, password=pw)
+
+        if (user is None):
+            messages.info('Такого пользователя не существует!')
+            return redirect('login.html')
+
+        auth.login(request, user)
+        return redirect('/home')
+
+    return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'home.html')
